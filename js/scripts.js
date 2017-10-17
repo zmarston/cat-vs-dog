4df173e474058aee711387b8ae08f6a86b84c10d
Chris Quattrocchi and Zachary Marston <chris@>
2017-10-17 14:44:08 -0700

    build page w/ cat and dog dialogue using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("img#dogpic").click(function() {
+    $("ul.dogtag").prepend("<li>WOOF WOOF!</li>");
+    $("ul.cattag").prepend("<li>HISSSSsss!</li>");
+  });
+  $("img#catpic").click(function() {
+    $("ul.cattag").prepend("<li>MrrroooWWooW!</li>");
+    $("ul.dogtag").prepend("<li>Bark!</li>");
+  });
+  $(".boneclick").click(function() {
+    $(".bone-showing").toggle();
+    $(".bone-hidden").toggle();
+  });
+  $(".birdclick").click(function() {
+    $(".bird-showing").toggle();
+    $(".bird-hidden").toggle();
+  });
+  $("img#bonepic").click(function() {
+    $("ul.dogtag").prepend("<li>*wags tail*</li>");
+  });
+  $("img#birdpic").click(function() {
+    $("ul.cattag").prepend("<li>Prrrrrrr</li>");
+  });
+});
